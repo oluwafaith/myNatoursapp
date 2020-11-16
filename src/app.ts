ab200bb3a6a38b1d3b2ba54976a0b588f2b788eb
Okpala Faith <okpalafaith1@gmail.com>
2020-11-16 21:58:51 +0100

    converted the boiler plate from js to ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError, { HttpError } from 'http-errors';
+import express, { Request, Response, NextFunction} from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err:HttpError, req:Request, res:Response, next:NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
